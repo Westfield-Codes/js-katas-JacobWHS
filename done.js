6eb690bafdb8e66419ef14b2f0084f90e7baabfe
JacobWHS <jacob127871@student-schoolsofwestfield.org>
2024-09-17 12:07:32 -0400

    🗿 - Added Reddit

--- done.js
+++ done.js
@@ -3,7 +3,30 @@
 /* COLOR KATAS */
 
 /* No Prompt */
+/* Variable and Alert Katas */
+// These katas do not involve functions and are very basic. 
 
+/* COLOR KATAS */
+
+/* Var Alert */
+// make a string variable for a color, set it equal to your favorite
+var color = "red";
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is " + color);
+
+/* Var Alert Prompt */
+// make a string variable for color, prompt the user for their favorite
+color = prompt("What's yours?");
+// alert color " is a nice color!"
+alert(color + " is a nice color! :)");
+
+/* Var Alert Prompt Conditional */
+// make a variable for color, prompt the user for their favorite
+color = prompt("What's your favorite color?");
+// if user enters "black" alert "Black is not really a color." 
+if (color == "black") alert("Black isn't really a color :p");
+// otherwise alert color " is a nice color!"
+else alert(color + " is a nice color! :3");
 
 /* Prompt */
 
